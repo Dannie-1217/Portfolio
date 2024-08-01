d5e8b786f25c67d8f72499c9a4db402a5406c330
Dannie <danni_chen@sfu.ca>
2024-08-01 00:03:26 -0700

    update homepage heading padding

--- public_html/js/navigation-mobile.js
+++ public_html/js/navigation-mobile.js
@@ -6,12 +6,25 @@ const mobile = document.querySelector('.mobile-menu');
 
 const logo =document.querySelector('.logo');
 
+const menu_elements=document.querySelectorAll('.mobile-menu-element');
+
+
 
 hamburger.addEventListener(
     'click', function(){
         mobile.classList.toggle('active');
     }
 )
+
+
+for(let element of menu_elements){
+    element.addEventListener(
+        'click',function(){
+            mobile.classList.remove('active');
+        }
+    )
+}
+
 
 window.addEventListener(
     'resize',function(){
